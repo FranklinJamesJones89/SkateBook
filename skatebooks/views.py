8994c47dfd60cdfe92c55e7ceaef06c69cc5ae3d
FranklinJamesJones89 <fjones89@icloud.com>
2023-03-22 16:00:30 -0400

    Add client-side query for Spot 'city/state' attributre

--- skatebooks/views.py
+++ skatebooks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.contrib import messages
 from .models import User, Spot
 from . forms import MyUserCreationForm, SpotForm
@@ -9,6 +10,14 @@ from . forms import MyUserCreationForm, SpotForm
 
 #@login_required(login_url = 'skatebooks: signup')
 def index(request):
+    if 'q' in request.GET:
+       q = request.GET['q'] 
+       #spots = Spot.objects.filter(city__icontains = q)
+       multiple_q = Q(Q(city__icontains = q) | Q(state__icontains = q))
+       spots = Spot.objects.filter(multiple_q)
+    else:
+        spots = Spot.objects.all()
+
     form = SpotForm()
 
     if request.method == 'POST':
@@ -21,7 +30,6 @@ def index(request):
 
             return redirect('skatebooks:index')
 
-    spots = Spot.objects.all()
     context = {'spots': spots, 'form': form}
 
     return render(request, 'skatebooks/index.html', context)
@@ -50,7 +58,7 @@ def signin(request):
 
     context = {'page': page}
     
-    return render(request, 'skatebooks/signup_form.html', context)
+    return render(request, 'skatebooks/components/forms/signup_form.html', context)
 
 def signup(request):
     form = MyUserCreationForm()
@@ -71,7 +79,7 @@ def signup(request):
 
     context= {'form': form}
             
-    return render(request, 'skatebooks/signup_form.html', context)
+    return render(request, 'skatebooks/components/forms/signup_form.html', context)
 
 def signout(request):
     logout(request)

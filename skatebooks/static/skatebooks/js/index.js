634fc1e00c398d30f618b5b13b71dfcc2b19cf9d
FranklinJamesJones89 <fjones89@icloud.com>
2023-04-11 14:11:59 -0400

    Modify profile.html

--- skatebooks/static/skatebooks/js/index.js
+++ skatebooks/static/skatebooks/js/index.js
@@ -1,3 +1,6 @@
+$(document).ready(function() {
+});
+
 spot_like_btn = document.getElementById('spot-like-btn');
 
 spot_like_btn.addEventListener('click', function(event) {
@@ -5,3 +8,5 @@ spot_like_btn.addEventListener('click', function(event) {
 	let targetUrl = spot_like_btn.getAttribute('href');
 	window.location.href = targetUrl;
 });
+
+
